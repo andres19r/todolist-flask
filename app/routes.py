e8a924de651a02ef7903acf4e601fd41ad2a266f
Andres Rivero <andres19rivero@gmail.com>
2022-06-02 21:33:35 -0400

    feat: added pagination

--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,22 @@
 from app import app, db
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
 from app.forms import CreateTaskForm, EmptyForm
 from app.models import Task
 
 
 @app.route('/')
 def index():
+    page = request.args.get('page', 1, type=int)
     tasks = Task.query.filter_by(status=False) \
-            .order_by(Task.created.desc()).all()
+            .order_by(Task.created.desc()) \
+            .paginate(page, app.config['TASKS_PER_PAGE'], False)
+    next_url = url_for('index', page=tasks.next_num) \
+        if tasks.has_next else None
+    prev_url = url_for('index', page=tasks.prev_num) \
+        if tasks.has_prev else None
     form = EmptyForm()
-    return render_template('index.html', tasks=tasks, form=form)
+    return render_template('index.html', tasks=tasks.items, form=form,
+                           next_url=next_url, prev_url=prev_url)
 
 @app.route('/create', methods=['GET', 'POST'])
 def create():
@@ -39,6 +46,13 @@ def complete(task):
 
 @app.route('/completed_tasks')
 def completed_tasks():
+    page = request.args.get('page', 1, type=int)
     tasks = Task.query.filter_by(status=True) \
-            .order_by(Task.completed.desc()).all()
-    return render_template('index.html', tasks=tasks)
+            .order_by(Task.completed.desc()) \
+            .paginate(page, app.config['TASKS_PER_PAGE'], False)
+    next_url = url_for('index', page=tasks.next_num) \
+        if tasks.has_next else None
+    prev_url = url_for('index', page=tasks.prev_num) \
+        if tasks.has_prev else None
+    return render_template('index.html', tasks=tasks.items,
+                           next_url=next_url, prev_url=prev_url)
